6b775dfc9ee8199316b72703f77eb2015c26692b
M147D <david.rmig@gmail.com>
2025-02-27 13:49:00 -0500

    Created new DTOs

---
+++ Servidor_Sistema_Geologia/DTO/CreateFosilDto.cs
@@ -0,0 +1,32 @@
+﻿namespace Servidor_Sistema_Geologia.DTO
+{
+	public class CreateFosilDto
+	{
+		// Propiedades propias del fósil
+		public string? Especie { get; set; }
+		public string? Periodo { get; set; }
+		public string? Nombre { get; set; }
+		public int? Edad { get; set; }
+		public string? Donante { get; set; }
+		public int? FechaIngreso { get; set; }
+		public string? Codigo { get; set; }
+		public int? Ejemplares { get; set; }
+		public string? DocumentosRelacionados { get; set; }
+		public string? LaminaURL { get; set; }
+		public bool? LaminaExiste { get; set; }
+
+		// Datos de ubicación
+		public string? Latitud { get; set; }
+		public string? Longitud { get; set; }
+		public string? Localidad { get; set; }
+		public string? Leyenda { get; set; }
+		public string? NombreProvincia { get; set; }
+		public string? NombrePais { get; set; }
+
+		// Estado
+		public string? DescripcionEstado { get; set; }
+
+		// Usuario para registro de acceso
+		public int UsuarioId { get; set; }
+	}
+}
---
+++ Servidor_Sistema_Geologia/DTO/CreateRocaDto.cs
@@ -0,0 +1,32 @@
+﻿namespace Servidor_Sistema_Geologia.DTO
+{
+	public class CreateRocaDto
+	{
+		// Propiedades propias de la roca
+		public string? TipoRoca { get; set; }
+		public string? Litologia { get; set; }
+		public string? Nombre { get; set; }
+		public int? Edad { get; set; }
+		public string? Donante { get; set; }
+		public int? FechaIngreso { get; set; }
+		public string? Codigo { get; set; }
+		public int? Ejemplares { get; set; }
+		public string? DocumentosRelacionados { get; set; }
+		public string? LaminaURL { get; set; }
+		public bool? LaminaExiste { get; set; }
+
+		// Datos de ubicación
+		public string? Latitud { get; set; }
+		public string? Longitud { get; set; }
+		public string? Localidad { get; set; }
+		public string? Leyenda { get; set; }
+		public string? NombreProvincia { get; set; }
+		public string? NombrePais { get; set; }
+
+		// Estado
+		public string? DescripcionEstado { get; set; }
+
+		// Usuario para registro de acceso
+		public int UsuarioId { get; set; }
+	}
+}
