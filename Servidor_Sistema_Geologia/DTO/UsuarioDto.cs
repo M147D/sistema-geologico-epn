22c0e5da561c36f1c08e31172c08b60bde94a34c
M147D <david.rmig@gmail.com>
2025-03-05 22:15:15 -0500

    Update identation

--- Servidor_Sistema_Geologia/DTO/UsuarioDto.cs
+++ Servidor_Sistema_Geologia/DTO/UsuarioDto.cs
@@ -5,13 +5,9 @@ namespace Servidor_Sistema_Geologia.DTO
 	public class UsuarioDto
 	{
 		public int Id { get; set; }
-
 		public string? NombreCompleto { get; set; }
-
 		public string? NombreUsuario { get; set; }
-
 		public string? Email { get; set; }
-
 		public RolUsuario Rol { get; set; }
 	}
 }
