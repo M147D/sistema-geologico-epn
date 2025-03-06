7c338bb98c135f0ab811dc6cb296965b06e683b8
M147D <david.rmig@gmail.com>
2025-03-05 21:36:33 -0500

    Refactor: type data and properties

--- Servidor_Sistema_Geologia/DTO/CreateFosilDto.cs
+++ Servidor_Sistema_Geologia/DTO/CreateFosilDto.cs
@@ -6,10 +6,10 @@
 		public string? Especie { get; set; }
 		public string? Periodo { get; set; }
 		public string? Nombre { get; set; }
-		public int? Edad { get; set; }
+		public ulong? Edad { get; set; }
 		public string? Donante { get; set; }
 		public string? Codigo { get; set; }
-		public int? Ejemplares { get; set; }
+		public uint? Ejemplares { get; set; }
 		public string? DocumentosRelacionados { get; set; }
 		public string? LaminaURL { get; set; }
 		public bool? LaminaExiste { get; set; }
@@ -21,6 +21,9 @@
 		public string? Leyenda { get; set; }
 		public string? NombreProvincia { get; set; }
 		public string? NombrePais { get; set; }
+
+		// Fotos 
+		public List<CreateFotoElementoDto> Fotos { get; set; } = new List<CreateFotoElementoDto>();
 
 		// Usuario para registro de acceso
 		public int UsuarioId { get; set; }
