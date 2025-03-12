0050d8306e3a9b3d98981bd17ad1dc66a5654f1f
M147D <david.rmig@gmail.com>
2025-03-12 15:23:37 -0500

    Refactor properties of foto

--- Servidor_Sistema_Geologia/DTO/CreateFotoElementoDto.cs
+++ Servidor_Sistema_Geologia/DTO/CreateFotoElementoDto.cs
@@ -2,6 +2,7 @@
 {
 	public class CreateFotoElementoDto
 	{
+		public IFormFile? ImagenFile { get; set; }
 		public byte[]? Imagen { get; set; }
 		public string? TipoFoto { get; set; }
 		public string? DescripcionEspecifica { get; set; }
