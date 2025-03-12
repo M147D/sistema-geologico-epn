19845fbdfc212e77546081faaf33ddf65debd05a
M147D <david.rmig@gmail.com>
2025-03-12 15:22:12 -0500

    Add controller and service by foto

---
+++ Servidor_Sistema_Geologia/Application/IFotoService.cs
@@ -0,0 +1,16 @@
+﻿using Servidor_Sistema_Geologia.Models;
+using Servidor_Sistema_Geologia.DTO;
+
+namespace Servidor_Sistema_Geologia.Application
+{
+	public interface IFotoService<TFoto, TReadFotoDto, TCreateFotoDto>
+		where TFoto : FotoElemento
+		where TCreateFotoDto : CreateFotoElementoDto
+	{
+		Task<TReadFotoDto> GetByIdAsync(int id);
+		Task<IEnumerable<TReadFotoDto>> GetAllAsync();
+		Task<TFoto> CreateAsync(TCreateFotoDto fotoDto, int galeriaId, string userName);
+		Task<TFoto> UpdateAsync(int id, TCreateFotoDto dto, int usuarioId);
+		Task DeleteAsync(int id, int usuarioId);
+	}
+}
